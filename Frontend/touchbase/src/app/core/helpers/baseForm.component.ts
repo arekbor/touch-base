f2e0651a9f4818867fa3d250dcd70f456f04ab3d
Arek <86869559+arekbor@users.noreply.github.com>
2024-03-20 00:02:34 +0100

    Update baseForm.component.ts
    remove console log

--- Frontend/touchbase/src/app/core/helpers/baseForm.component.ts
+++ Frontend/touchbase/src/app/core/helpers/baseForm.component.ts
@@ -6,7 +6,6 @@ export abstract class BaseFormComponent extends BaseComponent {
     form: AbstractControl,
     field: string
   ): ValidationErrors | null {
-    console.log("execute field errors");
     const control = form.get(field);
     if (control && control.invalid && (control.dirty || control.touched)) {
       return control.errors;
