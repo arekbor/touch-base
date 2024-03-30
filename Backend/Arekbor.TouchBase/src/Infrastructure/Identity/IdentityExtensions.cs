f195dafea2ef473149805323e050a87c686c9b61
arekbor <arekindependent@gmail.com>
2024-03-30 19:21:04 +0100

    refactor infrastructure

---
+++ Backend/Arekbor.TouchBase/src/Infrastructure/Identity/IdentityExtensions.cs
@@ -0,0 +1,43 @@
+using System.Text;
+using Arekbor.TouchBase.Infrastructure.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Arekbor.TouchBase.Infrastructure.Identity;
+
+public static class IdentityExtensions
+{
+    public static IServiceCollection AddIdentityConfigure(this IServiceCollection services)
+    {
+        var jwtOptions = services
+            .BuildServiceProvider()
+            .GetRequiredService<IOptions<JwtOptions>>().Value;
+
+        var keyBytes = Encoding.ASCII.GetBytes(jwtOptions.Secret);
+
+        services.AddAuthentication(options => {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(config => {
+            config.SaveToken = true;
+            config.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = jwtOptions.Issuer,
+                ValidAudience = jwtOptions.Audience,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+                ClockSkew = TimeSpan.Zero,
+            };
+        });
+
+        services.AddAuthorizationBuilder();
+        
+        return services;
+    }
+}
---
+++ Backend/Arekbor.TouchBase/src/Infrastructure/Persistence/PersistenceExtensions.cs
@@ -0,0 +1,23 @@
+using Arekbor.TouchBase.Infrastructure.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Arekbor.TouchBase.Infrastructure.Persistence;
+
+public static class PersistenceExtensions
+{
+    public static IServiceCollection AddPersistenceConfigure(this IServiceCollection services) 
+    {
+        var persistenceOptions = services
+            .BuildServiceProvider()
+            .GetRequiredService<IOptions<PersistenceOptions>>().Value;
+
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        services.AddDbContext<ApplicationDbContext>(options => {
+            options.UseNpgsql(persistenceOptions.Postgres, o => o.UseNodaTime());
+        });
+        
+        return services;
+    }
+}
