c89ce7c4b2f0aa6710e8485819c53bd1611726fd
arekbor <arekindependent@gmail.com>
2024-03-30 19:34:15 +0100

    disable required cors options

--- Backend/Arekbor.TouchBase/src/Infrastructure/Options/CorsOptions.cs
+++ Backend/Arekbor.TouchBase/src/Infrastructure/Options/CorsOptions.cs
@@ -5,16 +5,11 @@ namespace Arekbor.TouchBase.Infrastructure.Options;
 public class CorsOptions
 {
     public const string Position = "Cors";
-    [Required]
     [Url]
     public string AllowedOrigins { get; init; } = "";
-    [Required]
     public string AllowedMethods { get; init; } = "";
-    [Required]
     public string AllowedHeaders { get; init; } = "";
-    [Required]
     public bool AllowCredentials { get; init; }
-    [Required]
     [Range(0, 100000)]
     public int MaxAgeInSeconds { get; init; }
 }
